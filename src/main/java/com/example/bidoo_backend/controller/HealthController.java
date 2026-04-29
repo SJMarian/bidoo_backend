4d9a608398ceb228bd705fdfec9e2eb1c702d9b3
SJMarian <sabrinajahan968@gmail.com>
2026-04-29 21:51:40 +0600

    healthchange

--- src/main/java/com/example/bidoo_backend/controller/HealthController.java
+++ src/main/java/com/example/bidoo_backend/controller/HealthController.java
@@ -1,5 +1,18 @@
 package com.example.bidoo_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
 
+    @GetMapping("/")
+    public String home() {
+        return "Bidoo backend is running successfully";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
 }
